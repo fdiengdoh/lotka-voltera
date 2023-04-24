e6a4f7370f4fdb41943997a2cdcb37222b5c5c69
Farlando Diengdoh <fdiengdoh@fdiengdoh.com>
2023-04-24 13:10:23 +0530

    Update xy_svg.sh

--- src/xy_svg.sh
+++ src/xy_svg.sh
@@ -6,7 +6,7 @@ set title "[X]/[Y] vs Time Plot"
 set xlabel "Time (t)"
 set ylabel "[X]/[Y]"
 set yrange [0:450]
-plot "lv-res.dat" u (column(0)):2 w l lw 3 lc rgb '#9400d4' title "[X]","lv-res.dat" u (column(0)):3 w l lw 3 lc rgb '#0060ad' title "[Y]"
+plot "lv-res.dat" u (column(0)):2 w l lw 3 title "[X]","lv-res.dat" u (column(0)):3 w l lw 3 title "[Y]"
 
 
 set terminal svg enhanced font 'Arial,11'
@@ -14,4 +14,4 @@ set title "[X] vs [Y] Plot"
 set xlabel "[X]"
 set ylabel "[Y]"
 set output "rk2.svg"
-plot "lv-res.dat" using 2:3 w l lw 3 lc rgb '#0060ad' title ""
+plot "lv-res.dat" using 2:3 w l lw 3 title ""
